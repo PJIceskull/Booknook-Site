c4a860674e5555f92a625a7eea1a7eab31601c63
Pierce Issah <PJIceskull2003@gmail.com>
2023-10-19 12:33:40 -0400

    completed mobile nav bar

--- app/app.js
+++ app/app.js
@@ -19,7 +19,6 @@ function changeRoute() {
   }
 }
 
-
 /**** responsive nav 
 
 function initListeners() {
@@ -51,58 +50,98 @@ function initListeners() {
     }
   });
 }
-**/
-  
-
+// **/
 
 function initListeners() {
+  let displayMobNav = false;
+
+  $(window).on("resize", function (e) {
+    //console.log($(window).width());
+
+    // PRint to console the window size
+    // console.log($(window).width());
+
+    if ($(window).width() >= 768) {
+      // CLOSE humburger menu when Window.WIDTH increases
+      $(".nav-links").removeClass("mob-nav");
+      $(".nav-links").addClass("hide");
+      displayMobNav = false;
+    } else {
+      $(".nav-links").removeClass("hide");
+    }
+    // Hide navLinks when window is smaller than 768
+    if ($(window).width() <= 768) {
+      $(".nav-links").addClass("hide");
+      $(".hamburger-nav").removeClass("hide");
+    } else {
+      $(".hamburger-nav").addClass("hide");
+    }
+  });
+
+  $(".hamburger-nav").on("click", function (e) {
+    if (displayMobNav) {
+      $(".nav-links").removeClass("mob-nav");
+      $(".nav-links").addClass("hide");
+      displayMobNav = false;
+    } else {
+      $(".nav-links").addClass("mob-nav");
+      $(".mob-nav").removeClass("hide");
+      displayMobNav = true;
+    }
+  });
+
+  // Close mob nav while user clciks on Nav link
+  $("a").on("click", function (e) {
+    if (displayMobNav) {
+      $(".nav-links").addClass("hide");
+      $(".nav-links").removeClass("mob-nav");
+      displayMobNav = false;
+    }
+  });
+
+  // Modal
   $("#read-me").on("click", (e) => {
-      $("#modal").toggle();
+    $("#modal").toggle();
   });
 
   //cancel button
   $(".close").on("click", (e) => {
-      $("#modal").toggle();
+    $("#modal").toggle();
   });
-
 }
-
 
 function initURLListener() {
   $(window).on("hashchange", changeRoute);
   changeRoute();
 }
 
-
 // modal
 function initModalListeners() {
   $("#login").on("click", (e) => {
-      e.preventDefault();
-      $("#modal").addClass("active");
+    e.preventDefault();
+    $("#modal").addClass("active");
   });
 
   // Cancel button
   $(".close").on("click", (e) => {
-      $("#modal").removeClass("active");
+    $("#modal").removeClass("active");
   });
 
   //submit
   $("#close-cancel").on("click", (e) => {
-      Swal.fire({
-          position: 'center',
-          icon: 'success',
-          title: 'Login Successful!'
-        });
-      
-        //hide modal on login
-      $("#modal").removeClass("active");
+    Swal.fire({
+      position: "center",
+      icon: "success",
+      title: "Login Successful!",
+    });
+
+    //hide modal on login
+    $("#modal").removeClass("active");
   });
-  
 }
 
-  
 $(document).ready(function () {
-  initModalListeners();
   initListeners();
+  initModalListeners();
   initURLListener();
 });
